54d70afb2d127d2401ca220c254bc28b84f2e48e
Kyle Malloy <kmalloy24@gmail.com>
2024-08-02 10:11:29 -0400

    starlight tailwind

--- skeleton-starlight/apps/starlight/astro.config.mjs
+++ skeleton-starlight/apps/starlight/astro.config.mjs
@@ -1,28 +1,44 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import svelte from '@astrojs/svelte';
 
-import svelte from "@astrojs/svelte";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'My Docs',
-    social: {
-      github: 'https://github.com/withastro/starlight'
-    },
-    sidebar: [{
-      label: 'Guides',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Example Guide',
-        slug: 'guides/example'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }]
-  }), svelte()]
+  integrations: [
+    starlight({
+      customCss: [
+        // Path to your Tailwind base styles:
+        './src/tailwind.css'
+      ],
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/withastro/starlight'
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: 'Example Guide',
+              slug: 'guides/example'
+            }
+          ]
+        },
+        {
+          label: 'Reference',
+          autogenerate: {
+            directory: 'reference'
+          }
+        }
+      ]
+    }),
+    svelte(),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false
+    })
+  ]
 });
